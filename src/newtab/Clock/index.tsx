ca054cf846c7b46d85f10231fcf17f98d53f3d86
Patryk Podworski <patryk.podworski@gmail.com>
2025-09-28 20:24:50 +0200

    feat: add version display to new tab page

--- src/newtab/Clock/index.tsx
+++ src/newtab/Clock/index.tsx
@@ -5,7 +5,7 @@ export const Clock = () => {
 
   return (
     <div
-      className={`flex-1 flex flex-col items-center justify-center text-center text-white`}
+      className={`h-full flex flex-col items-center justify-center text-center text-white`}
     >
       <div className="text-6xl tracking-wider mb-4">{time}</div>
       <div className="text-xl text-white/80 tracking-wide">{date}</div>
